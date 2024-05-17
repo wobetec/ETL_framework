e23980e53aebf2ad8a717b4892856cfea20f4663
wobetec <esdras2821@gmail.com>
2024-05-17 03:39:57 -0300

    multiprocessing no simulator client

--- simulator/simulator_client.py
+++ simulator/simulator_client.py
@@ -1,3 +1,4 @@
+from multiprocessing import process
 import grpc
 import rpc.simulation_pb2 as simulation_pb2
 import rpc.simulation_pb2_grpc as simulation_pb2_grpc
@@ -6,6 +7,7 @@ from data_generator import Simulation
 
 import time
 import numpy as np
+import multiprocessing
 
 def send_event(event_data):
     with grpc.insecure_channel("localhost:50051") as channel:
@@ -14,7 +16,7 @@ def send_event(event_data):
         print("Resposta do servidor:", response.response_message)
 
 
-if __name__ == "__main__":
+def simulator_thread(user_min, user_max):
     start_time = time.perf_counter()
     simulation = Simulation()
     simulation.delete_folders()
@@ -29,3 +31,16 @@ if __name__ == "__main__":
         send_event(data)
         print(f"Execution step time: {time.perf_counter() - start_time}")
 
+
+if __name__ == "__main__":
+
+    N = 1
+    user_min = 500
+    user_max = 1000
+    delta = int((user_max - user_min) / N)
+
+    processes = []
+    for i in range(N):
+        p = multiprocessing.Process(target=simulator_thread, args=(user_min + i * delta, user_max + (i+1) * delta))
+        processes.append(p)
+        p.start()
